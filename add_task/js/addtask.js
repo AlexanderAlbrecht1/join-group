16f457848f4e8cb1238bc32191096f6f50ce7a47
joergdeymann <joergdeymann@web.de>
2024-08-23 21:09:43 +0200

    Update addtask.js

--- add_task/js/addtask.js
+++ add_task/js/addtask.js
@@ -1,6 +1,6 @@
-let tasks = [];
+let tasks =  [];
 
-function addNewTask() {
+async function addNewTask() {
     let title = document.getElementById('title');
     let description = document.getElementById('description');
     let assignedTo = Array.from(document.querySelectorAll('input[name="assign"]:checked'))
@@ -8,7 +8,7 @@ function addNewTask() {
     let dueDate = document.getElementById('due-date');
     let selectedPrio = document.querySelector('input[name="prio"]:checked');
     let category = document.getElementById('category');
-
+    tasks=await loadData('taskstorage');
     tasks.push({id: tasks.length, title: title.value, description: description.value, assignedTo: assignedTo, dueDate: dueDate.value, prio: selectedPrio.value, category: category.value});
     saveData('taskstorage', tasks);
     clearTaskInputs();

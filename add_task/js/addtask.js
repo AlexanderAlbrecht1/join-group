009ad10b339b05c2caa0978f7088002611546ec3
FiratTasyurdu <131069064+JNZN93@users.noreply.github.com>
2024-08-23 12:09:15 +0200

    edit inputfields

--- add_task/js/addtask.js
+++ add_task/js/addtask.js
@@ -3,21 +3,22 @@ let tasks = [];
 function addNewTask() {
     let title = document.getElementById('title');
     let description = document.getElementById('description');
-    let assignedTo = document.getElementById('assigned-to');
+    let assignedTo = Array.from(document.querySelectorAll('input[name="assign"]:checked'))
+                    .map(checkbox => checkbox.value);
     let dueDate = document.getElementById('due-date');
     let selectedPrio = document.querySelector('input[name="prio"]:checked');
     let category = document.getElementById('category');
 
-
-    tasks.push({title: title.value, description: description.value, assignedTo: assignedTo.value, dueDate: dueDate.value, prio: selectedPrio.value, category: category.value});
+    tasks.push({title: title.value, description: description.value, assignedTo: assignedTo, dueDate: dueDate.value, prio: selectedPrio.value, category: category.value});
 
 }
+
 
 function clearTaskInputs() {
     document.getElementById('title').value = "";
     document.getElementById('description').value = "";
-    document.getElementById('assigned-to').value = "";
+    document.querySelectorAll('input[name="assign"]').forEach(checkbox => checkbox.checked = false);
     document.getElementById('due-date').value = "";
-    document.querySelector('input[name="prio"]:checked').value = "";
-    document.getElementById('category').value = "";
+    document.querySelectorAll('input[name="prio"]').forEach(radio => radio.checked = false);
+    document.getElementById("category").selectedIndex = 0;
 }

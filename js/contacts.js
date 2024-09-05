165ed432bb52c0b1a92ca93d0a6c3f70a469526d
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2024-09-05 23:09:11 +0200

    css add contact

--- js/contacts.js
+++ js/contacts.js
@@ -249,10 +249,35 @@ function openCreateContactDialog() {
       </div>
       <div class="addContactInputContainer">
         <form onsubmit="addNewContact();return false;">
-            <input required id="name" type="text" placeholder="name" />
-            <input required id="email" type="email" placeholder="e-mail" />
-            <input id="phone" type="number" placeholder="phone number" />
-            <button type="submit">Add new contact "icon"</button>
+
+            <div id="input-field-container" class="input-container">
+               <input id="name" type="text" required placeholder="Name">
+               <span class="icon"  ><img src="./assets/img/desktop/person.svg"></span>
+               <span class="error-msg visible"></span>
+            </div>
+
+            <div id="input-field-container" class="input-container">
+               <input id="email" type="email" required placeholder="Email">
+               <span class="icon"  ><img src="./assets/img/desktop/letter.svg"></span>
+               <span class="error-msg visible"></span>
+            </div>
+            
+            <div id="input-field-container" class="input-container">
+               <input id="phone" type="number" required placeholder="Phone">
+               <span class="icon"  ><img src="./assets/img/desktop/phone.svg"></span>
+               <span class="error-msg visible"></span>
+            </div>
+
+            <div>
+               <div class="cancelButton">
+                  <span>Cancel </span>
+                  <div class="imgContainer"></div>
+               
+               </div>
+            
+
+            <button type="submit">Create contact</button>
+            </div>
         </form>
       </div>
     </div>

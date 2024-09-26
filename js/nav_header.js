45cf16c94a34c50278a910d7403a13eb78b5ff76
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2024-09-26 21:50:53 +0200

    bug fixed UserMongram Header

--- js/nav_header.js
+++ js/nav_header.js
@@ -7,3 +7,24 @@ function logOut() {
     sessionStorage.removeItem(PROJECT);
     isLogged();
 }
+
+function initSessionMonogram() {
+    isLogged();
+    logedUserMonogram();
+}
+
+/**
+ *
+ * returns the monogram of a Name
+ *
+ * @param {string} name first name and last name split by " "
+ * @returns the first characters of the 2 first Names in uppercase
+ */
+function getMonogram(name) {
+    let na = name.toUpperCase().split(' ', 2);
+    if (na.length == 1) {
+       return na[0][0];
+    } else {
+       return na[0][0] + na[1][0];
+    }
+ }

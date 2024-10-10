b14efb76e0f2e82090239f0a86d30615e049694e
AlexanderAlbrecht1 <alexander.albrecht1@gmail.com>
2024-10-10 17:01:08 +0200

    greeting user dashboard

--- js/dashboard.js
+++ js/dashboard.js
@@ -18,39 +18,33 @@ async function countTasksByPrio(prio) {
 
 async function countAllTasks() {
    const tasks = await loadData('taskstorage');
-
    if (!tasks || Object.keys(tasks).length === 0) {
       return 0;
    }
-
    const taskArray = Object.values(tasks).filter((task) => task !== null);
-
    return taskArray.length;
 }
 
-async function loadCurrentUserName(currentUserEmail) {
-   const users = await loadData('user');
-   if (!users) {
-      return 'Unknown User';
-   }
-   const userArray = Object.values(users);
-   if (currentUserEmail === 'guest') {
-      return 'Guest';
-   }
-   const user = userArray.find((u) => u.email === currentUserEmail);
-   return user ? user.user : 'Gast';
-}
+// async function loadCurrentUserName(currentUserEmail) {
+//    const users = await loadData('user');
+//    if (!users) {
+//       return 'Unknown User';
+//    }
+//    const userArray = Object.values(users);
+//    if (currentUserEmail === 'guest') {
+//       return 'Guest';
+//    }
+//    const user = userArray.find((u) => u.email === currentUserEmail);
+//    return user ? user.user : 'Gast';
+// }
 
 async function findNextDeadline() {
    const tasks = await loadObjectData('taskstorage');
    if (!tasks) return null;
-
    const taskArray = Object.values(tasks).filter(
       (task) => task && task.dueDate
    );
-
    const today = new Date();
-
    const upcomingTask = taskArray
       .map((task) => ({
          ...task,
@@ -58,7 +52,6 @@ async function findNextDeadline() {
       }))
       .filter((task) => task.dueDate >= today)
       .sort((a, b) => a.dueDate - b.dueDate)[0];
-
    return upcomingTask
       ? upcomingTask.dueDate.toLocaleDateString('en-US', {
            year: 'numeric',
@@ -77,7 +70,7 @@ async function updateDashboard() {
       const awaitingFeedbackCount = await countTasksByStatus('await-feedback');
       const totalCount = await countAllTasks();
       const currentUserEmail = localStorage.getItem('currentUserEmail');
-      const userName = await loadCurrentUserName(currentUserEmail);
+      // const userName = await loadCurrentUserName(currentUserEmail);
       const nextDeadline = await findNextDeadline();
 
       document.getElementById('todo-count').innerText = todoCount;
@@ -87,9 +80,7 @@ async function updateDashboard() {
       document.getElementById('awaiting-feedback-count').innerText =
          awaitingFeedbackCount;
       document.getElementById('total-count').innerText = totalCount;
-      document.querySelector(
-         '.greeting-text'
-      ).innerHTML = `Good Morning, <span class="dashboardusername" id="user-name">${userName}</span>`;
+      
       const deadlineElement = document.querySelector('.stats-text-deadline h3');
       deadlineElement.innerText = nextDeadline
          ? nextDeadline
@@ -104,6 +95,7 @@ async function updateDashboard() {
 function init() {
    if (isLogged()) {
       logedUserMonogram();
+      greetingUser();
       updateDashboard();
    }
 }
@@ -111,3 +103,18 @@ function init() {
 function forwardingToBoard() {
    window.location.href = './kanbanboard.html';
 }
+
+function greetingUser() {
+   salutation();
+   let data= JSON.parse(sessionStorage.getItem(PROJECT));  
+   let userName = data.username; 
+   document.getElementById('userName').innerHTML = '';
+   document.getElementById('userName').innerHTML = userName;
+}
+
+function salutation() {
+   let hello = getGreeting(greetingPos=getGreetingPos());
+   document.getElementById('greetingSalutation').innerHTML = '';
+   document.getElementById('greetingSalutation').innerHTML = hello;
+}
+
